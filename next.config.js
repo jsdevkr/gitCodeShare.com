65cb2566e16582827a283f1ecdcc051001f541c6
Thomas Jang <tom@axisj.com>
2018-08-09 00:40:29 +0900

    - change config for sass-loader and less-loader - antd testing

--- next.config.js
+++ next.config.js
@@ -17,6 +17,13 @@ module.exports = withPlugins([
       },
     },
   ],
-  [withLess],
   [withSass],
+  [
+    withLess,
+    {
+      lessLoaderOptions: {
+        javascriptEnabled: true,
+      },
+    },
+  ],
 ]);
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,99 @@
 import * as React from 'react';
-import '../assets/styles/styles.less';
-import '../assets/styles/styles.scss';
+import '../assets/styles/app.ts';
+import { Button, Icon, Card, Form, Input, Checkbox } from 'antd';
+import { FormComponentProps } from 'antd/lib/form';
 
-class App extends React.Component<{}> {
+const FormItem = Form.Item;
+
+interface IProps extends FormComponentProps {}
+
+class App extends React.Component<IProps> {
+  handleSubmit = e => {
+    e.preventDefault();
+    this.props.form.validateFields((err, values) => {
+      if (!err) {
+        console.log('Received values of form: ', values);
+      }
+    });
+  };
+
   render() {
-    return <div>Welcome to next.js!!!</div>;
+    const { getFieldDecorator } = this.props.form;
+
+    return (
+      <div>
+        <Card
+          title={
+            <span>
+              <Icon type="facebook" /> TITLE
+            </span>
+          }
+          extra={<a href="#">More</a>}
+          style={{ width: 300 }}
+        >
+          <p>Card content</p>
+          <p>Card content</p>
+          <p>Card content</p>
+
+          <Button type="primary">Primary</Button>
+          <Button>Default</Button>
+          <Button type="dashed">Dashed</Button>
+          <Button type="danger">Danger</Button>
+        </Card>
+
+        <Card style={{ width: 300 }}>
+          <Form onSubmit={this.handleSubmit} className="login-form">
+            <FormItem>
+              {getFieldDecorator('userName', {
+                rules: [
+                  { required: true, message: 'Please input your username!' },
+                ],
+              })(
+                <Input
+                  prefix={
+                    <Icon type="user" style={{ color: 'rgba(0,0,0,.25)' }} />
+                  }
+                  placeholder="Username"
+                />,
+              )}
+            </FormItem>
+            <FormItem>
+              {getFieldDecorator('password', {
+                rules: [
+                  { required: true, message: 'Please input your Password!' },
+                ],
+              })(
+                <Input
+                  prefix={
+                    <Icon type="lock" style={{ color: 'rgba(0,0,0,.25)' }} />
+                  }
+                  type="password"
+                  placeholder="Password"
+                />,
+              )}
+            </FormItem>
+            <FormItem>
+              {getFieldDecorator('remember', {
+                valuePropName: 'checked',
+                initialValue: true,
+              })(<Checkbox>Remember me</Checkbox>)}
+              <a className="login-form-forgot" href="">
+                Forgot password
+              </a>
+              <Button
+                type="primary"
+                htmlType="submit"
+                className="login-form-button"
+              >
+                Log in
+              </Button>
+              Or <a href="">register now!</a>
+            </FormItem>
+          </Form>
+        </Card>
+      </div>
+    );
   }
 }
 
-export default App;
+export default Form.create()(App);
