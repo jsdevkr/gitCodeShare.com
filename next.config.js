e9d44e4adc99b97aabe436444ff2926551df9aba
Dongwoo Gim <gimdongwoo@gmail.com>
2018-08-08 22:36:13 +0900

    add less, sass

--- next.config.js
+++ next.config.js
@@ -1,11 +1,22 @@
+const withPlugins = require('next-compose-plugins');
+const withLess = require('@zeit/next-less');
+const withSass = require('@zeit/next-sass');
 const withTypescript = require('@zeit/next-typescript');
 const ForkTsCheckerWebpackPlugin = require('fork-ts-checker-webpack-plugin');
 
-module.exports = withTypescript({
-  webpack(config, options) {
-    // Do not run type checking twice:
-    if (options.isServer) config.plugins.push(new ForkTsCheckerWebpackPlugin());
+module.exports = withPlugins([
+  [
+    withTypescript,
+    {
+      webpack(config, options) {
+        // Do not run type checking twice:
+        if (options.isServer)
+          config.plugins.push(new ForkTsCheckerWebpackPlugin());
 
-    return config;
-  },
-});
+        return config;
+      },
+    },
+  ],
+  [withLess],
+  [withSass],
+]);
