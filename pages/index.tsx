0579afeb79213ff113f1d6b4bc713444cb748286
Dongwoo Gim <gimdongwoo@gmail.com>
2018-08-08 22:01:32 +0900

    test commit hook

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,7 +2,11 @@ import * as React from 'react';
 
 class App extends React.Component<{}> {
 	render() {
-		return <div>Welcome to next.js!!!</div>;
+		return <div>Welcome to next.js!
+
+
+      
+    </div>;
 	}
 }
 
