b9d1942e13a3ad500e89f63b79378b64b18c8aa2
Dongwoo Gim <gimdongwoo@gmail.com>
2018-08-20 00:21:57 +0900

    set favicon path

--- pages/_document.js
+++ pages/_document.js
@@ -13,6 +13,7 @@ export default class MyDocument extends Document {
     return (
       <html>
         <Head>
+          <link rel="shortcut icon" href="/static/favicon.ico" />
           <link rel="stylesheet" href="/_next/static/style.css" />
           {this.props.styleTags}
         </Head>
