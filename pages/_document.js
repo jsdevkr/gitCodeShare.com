4613d47e9b29111ad7a510f642502c6446d6255d
Dongwoo Gim <gimdongwoo@gmail.com>
2018-08-19 19:16:57 +0900

    add styled-components ssr

--- pages/_document.js
+++ pages/_document.js
@@ -1,11 +1,20 @@
 import Document, { Head, Main, NextScript } from 'next/document';
+import { ServerStyleSheet } from 'styled-components';
 
 export default class MyDocument extends Document {
+  static getInitialProps({ renderPage }) {
+    const sheet = new ServerStyleSheet();
+    const page = renderPage(App => props => sheet.collectStyles(<App {...props} />));
+    const styleTags = sheet.getStyleElement();
+    return { ...page, styleTags };
+  }
+
   render() {
     return (
       <html>
         <Head>
           <link rel="stylesheet" href="/_next/static/style.css" />
+          {this.props.styleTags}
         </Head>
         <body>
           <Main />
