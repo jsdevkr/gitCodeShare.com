98ebbb3eae1d973f4213935c25279021f88b7cc9
Thomas Jang <tom@axisj.com>
2018-08-26 00:08:45 +0900

    apply ThemeProvider to _document.js

--- pages/_document.js
+++ pages/_document.js
@@ -1,10 +1,18 @@
 import Document, { Head, Main, NextScript } from 'next/document';
 import { ServerStyleSheet } from 'styled-components';
+import { ThemeProvider, theme } from '../styledComponents';
+import '../assets/styles/app';
 
 export default class MyDocument extends Document {
   static getInitialProps({ renderPage }) {
     const sheet = new ServerStyleSheet();
-    const page = renderPage(App => props => sheet.collectStyles(<App {...props} />));
+    const page = renderPage(App => props =>
+      sheet.collectStyles(
+        <ThemeProvider theme={theme}>
+          <App {...props} />
+        </ThemeProvider>,
+      ),
+    );
     const styleTags = sheet.getStyleElement();
     return { ...page, styleTags };
   }
