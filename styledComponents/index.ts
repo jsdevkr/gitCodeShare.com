340259dc9c5061049e31400e213ade1b7bf1afe4
Thomas <tom@axisj.com>
2018-09-06 15:32:57 +0900

    add export keyframes

--- styledComponents/index.ts
+++ styledComponents/index.ts
@@ -1,3 +1,3 @@
-export { default as styled, ThemeProvider, withTheme } from 'styled-components';
+export { default as styled, ThemeProvider, withTheme, keyframes } from 'styled-components';
 export { default as theme } from './theme';
 export { RoundedButton } from './StyledButton';
