fcf4dcdebe9445941469958c1bcad3c7a6e0e476
Dongwoo Gim <gimdongwoo@gmail.com>
2018-09-07 20:12:29 +0900

    update node package

--- .babelrc.js
+++ .babelrc.js
@@ -3,7 +3,7 @@ const env = require('./env-config.js');
 module.exports = {
   presets: ['next/babel', '@zeit/next-typescript/babel'],
   plugins: [
-    'transform-decorators-legacy',
+    ['@babel/plugin-proposal-decorators', { legacy: true }],
     ['transform-define', env],
     [
       'styled-components',
