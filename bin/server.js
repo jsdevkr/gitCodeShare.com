880fdedd7c05fe64e81a81f24e29d3bb63153c5d
Dongwoo Gim <gimdongwoo@gmail.com>
2018-09-07 20:13:01 +0900

    add bin

---
+++ bin/server.js
@@ -0,0 +1,20 @@
+require('dotenv').config();
+
+// Transpile all code following this line with babel and use 'env' (aka ES6)
+require('ts-node/register');
+require('@babel/register')({
+  extensions: ['.ts'],
+  presets: [
+    [
+      '@babel/preset-env',
+      {
+        targets: {
+          node: 'current',
+        },
+      },
+    ],
+  ],
+});
+
+// Import the rest of our application.
+require('../server/server');
