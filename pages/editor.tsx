bc0793034a40e49c0189e3dc1707ac9d669c8d64
Chris Yang <ysm0622@gmail.com>
2018-09-08 19:37:53 +0900

    Add: theme, language select feature in editor page

--- pages/editor.tsx
+++ pages/editor.tsx
@@ -1,64 +1,77 @@
 import * as React from 'react';
 
 import { Controlled as CodeMirror } from 'react-codemirror2';
+import {
+  LANGUAGES,
+  THEMES,
+  DEFAULT_LANGUAGE,
+  DEFAULT_THEME,
+  DEFAULT_CODE,
+  DEFAULT_SETTINGS,
+} from '../common/constants';
+import { Menu, Dropdown, Button, Icon } from 'antd';
+import { EditorConfiguration } from 'codemirror';
+
+if (typeof navigator !== 'undefined') {
+  LANGUAGES.map(lang => require(`codemirror/mode/${lang.mode}/${lang.mode}`));
+}
 
 interface IEditorProps {}
-interface IEditorState {
-  value: string;
+interface IEditorState extends EditorConfiguration {
+  language: string;
 }
 
 class Editor extends React.Component<IEditorProps, IEditorState> {
   state = {
-    value: '',
+    ...DEFAULT_SETTINGS,
+    value: DEFAULT_CODE,
+    mode: DEFAULT_LANGUAGE.mode,
+    theme: DEFAULT_THEME.id,
+    language: DEFAULT_LANGUAGE.name,
+    themeName: DEFAULT_THEME.name,
+    lineNumbers: true,
+    scrollBarStyle: null,
+    viewportMargin: Infinity,
+    lineWrapping: true,
   };
 
   render() {
-    const config = {
-      paddingVertical: '48px',
-      paddingHorizontal: '32px',
-      marginVertical: '45px',
-      marginHorizontal: '45px',
-      backgroundImage: null,
-      backgroundImageSelection: null,
-      backgroundMode: 'color',
-      // backgroundColor: DEFAULT_BG_COLOR,
-      dropShadow: true,
-      dropShadowOffsetY: '20px',
-      dropShadowBlurRadius: '68px',
-      // theme: DEFAULT_THEME.id,
-      windowTheme: 'none',
-      // language: DEFAULT_LANGUAGE,
-      fontFamily: 'Hack',
-      fontSize: '14px',
-      lineHeight: '133%',
-      windowControls: true,
-      widthAdjustment: true,
-      lineNumbers: false,
-      exportSize: '2x',
-      titleBar: '',
-      watermark: false,
-      squaredImage: false,
-      timestamp: false,
+    const handleLanguageMenuClick = e => {
+      this.setState({ mode: e.item.props.menu.mode });
     };
-    const options = {
-      lineNumbers: true,
-      mode: 'plaintext',
-      theme: 'vs',
-      scrollBarStyle: null,
-      viewportMargin: Infinity,
-      lineWrapping: true,
-      extraKeys: { 'Shift-Tab': 'indentLess' }, // negative values removes the cursor, undefined means default (530)
-      cursorBlinkRate: -1,
+    const handleThemeMenuClick = e => {
+      this.setState({ theme: e.item.props.menu.id });
     };
+    const menu = (menuItems, clickHandler) => (
+      <Menu onClick={clickHandler}>
+        {menuItems.map(menu => (
+          <Menu.Item key={menu.name} menu={menu}>
+            {menu.name}
+          </Menu.Item>
+        ))}
+      </Menu>
+    );
     return (
-      <CodeMirror
-        className={`CodeMirror__container window-theme__${config.windowTheme}`}
-        onBeforeChange={(editor, data, value) => {
-          this.setState({ value });
-        }}
-        value={this.state.value}
-        options={options}
-      />
+      <>
+        <CodeMirror
+          className={`CodeMirror__container`}
+          onBeforeChange={(editor, data, value) => {
+            this.setState({ value });
+          }}
+          value={this.state.value}
+          options={this.state}
+        />
+        <Dropdown overlay={menu(THEMES, handleThemeMenuClick)} trigger={['click']}>
+          <Button style={{ marginLeft: 8 }}>
+            {this.state.themeName} <Icon type="down" />
+          </Button>
+        </Dropdown>
+        <Dropdown overlay={menu(LANGUAGES, handleLanguageMenuClick)} trigger={['click']}>
+          <Button style={{ marginLeft: 8 }}>
+            {this.state.language} <Icon type="down" />
+          </Button>
+        </Dropdown>
+      </>
     );
   }
 }
