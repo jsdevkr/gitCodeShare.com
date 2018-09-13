261fae5eae38031e2aacd438eb3c1c3ca67032d0
Wonho Kim <rhwhdeodhkd@naver.com>
2018-09-13 22:43:16 +0900

    add plugins-class-properties for decorator

--- .babelrc.js
+++ .babelrc.js
@@ -3,7 +3,18 @@ const env = require('./env-config.js');
 module.exports = {
   presets: ['next/babel', '@zeit/next-typescript/babel'],
   plugins: [
-    ['@babel/plugin-proposal-decorators', { legacy: true }],
+    [
+      '@babel/plugin-proposal-decorators',
+      {
+        legacy: true,
+      },
+    ],
+    [
+      '@babel/plugin-proposal-class-properties',
+      {
+        loose: true,
+      },
+    ],
     ['transform-define', env],
     [
       'styled-components',
