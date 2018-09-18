0e9bca54e2de10de1a74e74351dc1a4412a28b14
jiniC <jinic323@gmail.com>
2018-09-18 21:41:08 +0900

    add footer component

--- components/MainFooter.tsx
+++ components/MainFooter.tsx
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
-import { styled, MContainer } from '../styledComponents';
+import { styled, MContainer, MainNavMenu, FlexRightBox } from '../styledComponents';
 import { Layout } from 'antd';
+import Link from 'next/link';
 
 interface IProps {}
 
@@ -14,11 +15,54 @@ const FooterWrap = styled(Footer as any)`
   background-color: #fff;
 `;
 
+const FooterContainer = styled(MContainer as any)`
+  display: inherit;
+
+  address {
+    margin: auto 0;
+  }
+`;
+
+const RightBox = styled(FlexRightBox as any)`
+  display: inline-block;
+`;
+
 class MainFooter extends Component<IProps> {
   render() {
     return (
       <FooterWrap>
-        <MContainer>footer</MContainer>
+        <FooterContainer>
+          <address>GitCodeShare.com ⓒ 2018</address>
+          <RightBox>
+            <MainNavMenu mode="horizontal">
+              <MainNavMenu.Item>
+                <Link href="/">
+                  <a>JSDEVKR</a>
+                </Link>
+              </MainNavMenu.Item>
+              <MainNavMenu.Item>
+                <Link href="/">
+                  <a>Contributors</a>
+                </Link>
+              </MainNavMenu.Item>
+              <MainNavMenu.Item>
+                <Link href="/">
+                  <a>Github Repository</a>
+                </Link>
+              </MainNavMenu.Item>
+              <MainNavMenu.Item>
+                <Link href="/">
+                  <a>Terms of Service</a>
+                </Link>
+              </MainNavMenu.Item>
+              <MainNavMenu.Item>
+                <Link href="/">
+                  <a>Privacy Police</a>
+                </Link>
+              </MainNavMenu.Item>
+            </MainNavMenu>
+          </RightBox>
+        </FooterContainer>
       </FooterWrap>
     );
   }
