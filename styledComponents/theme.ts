2e594d0885006ca444c1cb4fa36922606ee9eed7
Wonho Kim <rhwhdeodhkd@naver.com>
2018-09-19 23:03:28 +0900

    add theme

--- styledComponents/theme.ts
+++ styledComponents/theme.ts
@@ -1,5 +1,17 @@
 const theme = {
   primaryColor: '#000',
+  primaryTextColor: '#fff',
+  colorPalette: {
+    blueCharcoal: '#222b33',
+    gullGray: '#a4adb4',
+    neonBlue: '#5147ff',
+    patternsBlue: '#dbe3e9',
+    blueViolet: '#9013fe',
+    deepMagenta: '#bd10e0',
+    gainsBoro: '#d8d8d8',
+    comet: '#5e616e',
+    silver: '#c7c5c5',
+  },
 };
 
 export default theme;
