9b41e01371570ece8d111899f99b4e0394970d7e
cindyu <songyi0207@gmail.com>
2018-10-01 02:54:43 +0900

    Add extension main page

--- styledComponents/StyledButton.tsx
+++ styledComponents/StyledButton.tsx
@@ -82,7 +82,33 @@ const DownloadButton: typeof RoundedButton = styled(RoundedButton as any)`
     }
   }
 `;
+const SignUpButton: typeof RoundedButton = styled(RoundedButton as any)`
+  &.ant-btn {
+    max-width: 350px;
+    width: 100%;
+    height: 50px;
+    background-color: #5147ff;
+    box-shadow: 0 2px 4px 0 #222b33;
+    border-color: ${props => props.theme.colorPalette.neonBlue};
+    [bnt-content] {
+      width: 170px;
+      height: 20px;
+      font-family: IBMPlexSans;
+      font-size: 16px;
+      font-weight: 500;
+      font-style: normal;
+      font-stretch: normal;
+      line-height: normal;
+      letter-spacing: normal;
+      text-align: center;
+    }
 
+    span {
+      font-size: 16px;
+      font-weight: 600;
+    }
+  }
+`;
 const LineButton: typeof RoundedButton = styled(RoundedButton as any)`
   &.ant-btn {
     max-width: 200px;
@@ -120,4 +146,13 @@ const DropDownButton: typeof RoundedButton = styled(RoundedButton as any)`
   }
 `;
 
-export { RoundedButton, GithubButton, BorderlessButton, DownloadButton, SmDownloadButton, LineButton, DropDownButton };
+export {
+  RoundedButton,
+  GithubButton,
+  BorderlessButton,
+  DownloadButton,
+  SmDownloadButton,
+  LineButton,
+  DropDownButton,
+  SignUpButton,
+};
