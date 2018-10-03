42bb8dfab7bbe4ecf007f5296a17c6a7c57e7dfa
Kelly Kim <mein2555@gmail.com>
2018-10-03 19:14:13 +0900

    Add proxyContext
    - Add proxyContext
    - Edit signupButton style
    - Add starred list page /starred

---
+++ pages/starred.tsx
@@ -0,0 +1,45 @@
+import * as React from 'react';
+import { default as ApiProvider } from '../providers/ApiProvider';
+import { IGist } from '../model/gist';
+import { SLayout } from '../styledComponents';
+
+interface IProps {
+  list: [];
+}
+
+class Starred extends React.Component<IProps> {
+  state = { list: [] };
+
+  componentDidMount() {
+    this.getStarred();
+  }
+
+  async getStarred() {
+    try {
+      const list = await ApiProvider.GistRequest.getStarredGists();
+      this.setState({ list });
+    } catch (err) {
+      this.setState({ list: [] });
+      console.log(err);
+    }
+  }
+
+  render() {
+    const { list } = this.state;
+    console.log(list);
+    return (
+      <SLayout>
+        <h1 style={{ color: 'white' }}>gitCodeShare</h1>
+        {list.map((gist: IGist, i: number) => {
+          return (
+            <p key={i} style={{ color: 'white' }}>
+              {gist.url}
+            </p>
+          );
+        })}
+      </SLayout>
+    );
+  }
+}
+
+export default Starred;
