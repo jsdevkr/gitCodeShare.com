c24ee35ec923e42dac13f3425f2557418237dc02
jiniC <jinic323@gmail.com>
2018-10-20 18:36:35 +0900

    change logo icon

---
+++ styledComponents/StyledImage/index.tsx
@@ -0,0 +1,10 @@
+import * as React from 'react';
+import styled from 'styled-components';
+
+const Logo = () => <span className="icon-logo-full" />;
+
+const SLogoImage = styled(Logo)`
+  font-size: 50px;
+`;
+
+export { SLogoImage };
