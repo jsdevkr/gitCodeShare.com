b906164addb49c7171b1473b780ed2d50e053f61
Thomas <tom@axisj.com>
2018-10-20 18:57:20 +0900

    add @types/styled-components, SLogoImage

--- styledComponents/StyledImage/index.tsx
+++ styledComponents/StyledImage/index.tsx
@@ -1,10 +1,14 @@
 import * as React from 'react';
 import styled from 'styled-components';
 
-const Logo = () => <span className="icon-logo-full" />;
+// const Logo: React.SFC<any> = () => <span className="icon-logo-full" style={{ fontSize: '50px' }} />;
 
-const SLogoImage = styled(Logo)`
-  font-size: 50px;
-`;
+const SLogoImage: React.SFC<any> = () => {
+  const IconSpan = styled.span`
+    font-size: 50px;
+  `;
+
+  return <IconSpan className="icon-logo-full" />;
+};
 
 export { SLogoImage };
