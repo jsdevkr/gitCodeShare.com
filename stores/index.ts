12e74fb7ce8cb2a5cef1e081b8bf892156072ce1
Dongwoo Gim <gimdongwoo@gmail.com>
2018-10-21 04:42:22 +0900

    add getInitialProps to index

--- stores/index.ts
+++ stores/index.ts
@@ -1,3 +1,9 @@
-export { default as AppStore } from './AppStore';
+import ApiProvider from '../providers/ApiProvider';
+
+export { default as AppStore, IAppStore } from './AppStore';
 export { default as create } from './create';
 export { default as StoreProvider } from './StoreProvider';
+
+export interface IStoreEnv {
+  provider: typeof ApiProvider;
+}
