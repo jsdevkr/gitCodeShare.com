a2deb9a6b5d85fd0545ef4ceda52cc5716461212
Kelly Kim <mein2555@gmail.com>
2018-10-23 16:25:04 +0900

    Add auth callback window.close();

--- server/handlers/auth.ts
+++ server/handlers/auth.ts
@@ -6,7 +6,13 @@ const router: Router = Router();
 router.get('/github', passport.authenticate('github'));
 router.get('/github/callback', passport.authenticate('github', { failureRedirect: '/' }), (req, res) => {
   req.session.save(() => {
-    res.redirect('/');
+    res.send(`<!DOCTYPE html>
+         <html>
+           <script type="text/javascript">
+             window.close();
+           </script>
+         </html>`);
+    // res.redirect('/');
     // res.send(
     //   `<!DOCTYPE html>
     //     <html>
