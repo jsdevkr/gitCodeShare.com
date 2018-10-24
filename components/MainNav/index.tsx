d11853a21bbffe769b6185040ec8b2bdce977772
Wonho Kim <rhwhdeodhkd@naver.com>
2018-10-24 19:21:12 +0900

    remove unused

--- components/MainNav/index.tsx
+++ components/MainNav/index.tsx
@@ -12,7 +12,6 @@ import {
 } from '../../styledComponents';
 import { Layout } from 'antd';
 import Link from 'next/link';
-import { ApiProvider } from '../../providers';
 
 interface IProps {
   className?: string;
