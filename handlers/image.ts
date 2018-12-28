3489b9e31569cab9e5eacd9f29e4f41aea4e7d69
Woo Gim <woo@chequer.io>
2018-12-28 15:45:31 +0900

    add debounce for limited resource

--- handlers/image.ts
+++ handlers/image.ts
@@ -1,82 +1,88 @@
 import puppeteer from 'puppeteer';
 import { Request, Response, NextFunction } from 'express';
 import crypto from 'crypto';
+import debounce from 'lodash.debounce';
+import ms from 'ms';
 import { cache } from './';
 
 const ttl = 60 * 5;
 const PORT = parseInt(process.env.FRONT_PORT, 10) || 3000;
 
 export default function(browser: { instance: puppeteer.Browser }) {
-  return async (req: Request, res: Response, next: NextFunction) => {
-    const { state, id } = req.query;
-    if (!state && !id) {
-      res.status(400).send();
-    }
+  return debounce(
+    async (req: Request, res: Response, next: NextFunction) => {
+      const { state, id } = req.query;
+      if (!state && !id) {
+        res.status(400).send();
+      }
 
-    try {
-      const buffer = await cache.wrap(
-        state
-          ? `image/code/${crypto
-              .createHash('sha1')
-              .update(state)
-              .digest('hex')}`
-          : `image/id/${id}`,
-        async () => {
-          let url: string = `http://localhost:${PORT}`;
-          switch (!!state) {
-            case true:
-              url += `/editor?state=${state}`;
-              break;
-            case false:
-              url += `/?${id}`;
-              break;
-            default:
-              res.status(400).send();
-              break;
-          }
-          console.log(url);
+      try {
+        const buffer = await cache.wrap(
+          state
+            ? `image/code/${crypto
+                .createHash('sha1')
+                .update(state)
+                .digest('hex')}`
+            : `image/id/${id}`,
+          async () => {
+            let url: string = `http://localhost:${PORT}`;
+            switch (!!state) {
+              case true:
+                url += `/editor?state=${state}`;
+                break;
+              case false:
+                url += `/?${id}`;
+                break;
+              default:
+                res.status(400).send();
+                break;
+            }
+            console.log(url);
 
-          const page = await browser.instance.newPage();
-          try {
-            await page.goto(url);
+            const page = await browser.instance.newPage();
+            try {
+              await page.goto(url);
 
-            const selector = 'div.CodeMirror';
-            await page.waitFor(selector);
+              const selector = 'div.CodeMirror';
+              await page.waitFor(selector);
 
-            const rect = await page.evaluate(_selector => {
-              const element = document.querySelector(_selector);
+              const rect = await page.evaluate(_selector => {
+                const element = document.querySelector(_selector);
 
-              const { width: beforeWidth } = element.getBoundingClientRect();
-              const adjustHeight = (beforeWidth / 500) * 261.11;
-              element.style.height = `${adjustHeight}px`;
+                const { width: beforeWidth } = element.getBoundingClientRect();
+                const adjustHeight = (beforeWidth / 500) * 261.11;
+                element.style.height = `${adjustHeight}px`;
 
-              const { x, y, width, height } = element.getBoundingClientRect();
-              return { left: x, top: y, width, height, id: element.id };
-            }, selector);
+                const { x, y, width, height } = element.getBoundingClientRect();
+                return { left: x, top: y, width, height, id: element.id };
+              }, selector);
 
-            return await page.screenshot({
-              clip: {
-                x: rect.left,
-                y: rect.top,
-                width: rect.width,
-                height: rect.height,
-              },
-            });
-          } catch (error) {
-            throw error;
-          } finally {
-            page.close();
-          }
-        },
-        { ttl },
-      );
+              return await page.screenshot({
+                clip: {
+                  x: rect.left,
+                  y: rect.top,
+                  width: rect.width,
+                  height: rect.height,
+                },
+              });
+            } catch (error) {
+              throw error;
+            } finally {
+              page.close();
+            }
+          },
+          { ttl },
+        );
 
-      res.set('Content-Type', 'image/png');
-      res.write(buffer, 'binary');
-      res.end(null, 'binary');
-    } catch (e) {
-      console.log('error', e);
-      res.status(500).send();
-    }
-  };
+        res.set('Content-Type', 'image/png');
+        res.write(buffer, 'binary');
+        res.end(null, 'binary');
+      } catch (e) {
+        console.log('error', e);
+        res.status(500).send();
+      }
+    },
+    ms('5s'),
+    { leading: true, trailing: true },
+  );
 }
