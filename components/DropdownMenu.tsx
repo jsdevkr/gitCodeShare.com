52c257e6e7248cc78445618e796ead4ee4401e6e
Dongwoo Gim <gimdongwoo@gmail.com>
2018-12-31 20:54:56 +0900

    fix build bug

--- components/DropdownMenu.tsx
+++ components/DropdownMenu.tsx
@@ -4,9 +4,7 @@ import { DropDownMenu as StyledDropDownMenu } from '../styledComponents';
 export const DropdownMenu = (menuItems, clickHandler) => (
   <StyledDropDownMenu onClick={clickHandler} theme="dark">
     {menuItems.map(menu => (
-      <StyledDropDownMenu.Item key={menu.name} menu={menu}>
-        {menu.name}
-      </StyledDropDownMenu.Item>
+      <StyledDropDownMenu.Item key={menu.name}>{menu.name}</StyledDropDownMenu.Item>
     ))}
   </StyledDropDownMenu>
 );
